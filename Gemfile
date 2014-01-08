96bf18ed313827a415c78371272ee4a124c78917
michaelredko <mir@qapint.com>
2014-01-08 12:46:16 +0200

    refactoring

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
+gem 'bootstrap-sass', '~> 3.0.3.0'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ FirstApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
-  root 'welcome#index'
 
-  resources :microposts
-
-  resources :users
+  root :to => redirect("static_pages/home")
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
